6a9fee00d4ddaaff1107a867a209b4ab03553d4d
Stephan Randle <stephanrandle@hotmail.com>
2023-01-18 21:11:03 -0500

    updated storybook preview

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,14 +1,43 @@
-import GlobalProvider from '@/context'
+import { useContext, useEffect } from 'react'
+import GlobalProvider, { GlobalContext } from '@/context'
 
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
 	controls: { matchers: { color: /(background|color)$/i, date: /Date$/ } },
 }
 
-const withThemeProvider = (Story, context) => (
-	<GlobalProvider>
-		<Story />
-	</GlobalProvider>
-)
+const withThemeProvider = (Story, context) => {
+	const GlobalUser = ({
+		children,
+		context: {
+			globals: { darkmode },
+		},
+	}) => {
+		const { setDarkmode } = useContext(GlobalContext)
+		useEffect(() => setDarkmode(darkmode), [darkmode])
+		return children
+	}
+	return (
+		<GlobalProvider>
+			<GlobalUser context={context}>
+				<Story />
+			</GlobalUser>
+		</GlobalProvider>
+	)
+}
+
+export const globalTypes = {
+	darkmode: {
+		toolbar: {
+			icon: 'circlehollow',
+			description: 'Customize theme mode',
+			dynamicTitle: true,
+			items: [
+				{ value: 'light', title: 'Light Mode', left: '☀️' },
+				{ value: 'dark', title: 'Dark Mode', left: '🌕' },
+			],
+		},
+	},
+}
 
 export const decorators = [withThemeProvider]
