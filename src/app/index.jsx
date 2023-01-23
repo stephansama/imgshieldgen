9ba4840e58d5a091405782bd8f1ef1a5f29db247
Stephan Randle <stephanrandle@hotmail.com>
2023-01-23 17:19:21 -0500

    refactored app entrypoint

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,32 +1,23 @@
-import { Box } from '@mui/material'
-import Draggable from 'react-draggable'
+import { Box, useTheme } from '@mui/material'
 
 import Navbar from '@components/Navbar'
 import Preview from '@components/Preview'
 import IconPicker from '@components/IconPicker'
 import BadgeBuilder from '@components/BadgeBuilder'
 
-import styles from './app.module.css'
-
 export default function App() {
+	const { GlobalStyles } = useTheme()
 	return (
 		<Box sx={{ display: 'flex', height: '100vh', flexDirection: 'column' }}>
 			<Navbar />
 			<Box sx={{ display: 'flex', flexGrow: 1, overflowY: 'hidden' }}>
 				<IconPicker />
 				<Preview>
-					<Draggable
-						bounds={{ top: -100, left: -100, right: 100, bottom: 100 }}
-						defaultPosition={{ x: 0, y: 0 }}
-					>
-						<Box className={styles[`preview-container`]}>
-							<img
-								className={styles[`preview-img`]}
-								src='https://api.netlify.com/api/v1/badges/a0210912-a859-4ea1-960c-9638b5fe0776/deploy-status'
-								alt='netlify'
-							/>
-						</Box>
-					</Draggable>
+					<img
+						className={GlobalStyles[`no-select`]}
+						src='https://api.netlify.com/api/v1/badges/a0210912-a859-4ea1-960c-9638b5fe0776/deploy-status'
+						alt='netlify'
+					/>
 				</Preview>
 				<BadgeBuilder />
 			</Box>
