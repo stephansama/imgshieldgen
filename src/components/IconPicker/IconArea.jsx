80fb5224acc395276da5203254247a8417a1d933
Stephan Randle <stephanrandle@hotmail.com>
2023-01-23 17:20:54 -0500

    connected icon section to global context

--- src/components/IconPicker/IconArea.jsx
+++ src/components/IconPicker/IconArea.jsx
@@ -1,11 +1,14 @@
 import { useContext } from 'react'
 import { Box, Tooltip } from '@mui/material'
 
+import { GlobalContext } from '@/context'
 import { IconPickerContext } from '.'
+
 import SimpleIcon from './SimpleIcon'
 
 export default function IconArea() {
 	const { shownIcons, lastIconRef } = useContext(IconPickerContext)
+	const { selectIcon } = useContext(GlobalContext)
 	return (
 		<Box
 			sx={{
@@ -17,19 +20,21 @@ export default function IconArea() {
 				justifyContent: 'center',
 			}}
 		>
-			{shownIcons.map((icon, i, allIcons) => (
+			{shownIcons.map((icon, i, totalArray) => (
 				<Tooltip followCursor key={i} title={icon.title}>
 					<Box
 						sx={{
 							p: '5px',
 							width: 'min-content',
 							height: 'min-content',
+							cursor: 'pointer',
 							bgcolor: '#fff',
 							display: 'flex',
 							alignItems: 'center',
 							justifyContent: 'center',
 						}}
-						ref={i === allIcons.length - 1 ? lastIconRef : null}
+						onClick={selectIcon(icon.slug)}
+						ref={i === totalArray.length - 1 ? lastIconRef : null}
 					>
 						<SimpleIcon icon={icon} colored />
 					</Box>
